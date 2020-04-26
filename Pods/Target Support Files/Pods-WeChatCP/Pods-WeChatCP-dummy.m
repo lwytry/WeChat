bb6f28977212fcdf851ab58a890511aaad1c707d
liwuyi <2020783236@qq.com>
2020-04-26 09:21:59 +0800

    添加忽略文件

--- Pods/Target Support Files/Pods-WeChatCP/Pods-WeChatCP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeChatCP : NSObject
-@end
-@implementation PodsDummy_Pods_WeChatCP
-@end
--- Pods/Target Support Files/YSMChineseSort/YSMChineseSort-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YSMChineseSort : NSObject
-@end
-@implementation PodsDummy_YSMChineseSort
-@end
