5bed11e81c9325dc0510985f82d18f1083bc2d6f
liwuyi <2020783236@qq.com>
2020-05-19 18:19:46 +0800

    remove

--- WeChatCP/view/chat/messageDisplayView/messageCellView/TextMessageCell.m
+++ WeChatCP/view/chat/messageDisplayView/messageCellView/TextMessageCell.m
@@ -31,42 +31,35 @@
 
 - (void)setMessage:(TextMessage *)message
 {
-    if (self.message && [self.message.ID isEqualToString:message.ID]) {
-        return;
-    }
-    MessageOwnerType lastOwnType = self.message ? self.message.ownerTyper : -1;
     [super setMessage:message];
     [self.messageLabel setAttributedText:[message attrText]];
     
     [self.messageLabel setContentCompressionResistancePriority:500 forAxis:UILayoutConstraintAxisHorizontal];
     [self.messageBackgroundView setContentCompressionResistancePriority:100 forAxis:UILayoutConstraintAxisHorizontal];
-    NSLog(@"lastOwnType----%ld, ownerTyper===%ld",(long)lastOwnType,  (long)message.ownerTyper);
-    if (lastOwnType != message.ownerTyper) {
-        if (message.ownerTyper == MessageOwnerTypeSelf) {
-            [self.messageBackgroundView setImage:[UIImage imageNamed:@"message_sender_bg"]];
-            [self.messageBackgroundView setHighlightedImage:[UIImage imageNamed:@"message_sender_bgHL"]];
-            
-            [self.messageLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
-                make.right.mas_equalTo(self.messageBackgroundView).mas_offset(-MSG_SPACE_RIGHT);
-                make.top.mas_equalTo(self.messageBackgroundView).mas_offset(MSG_SPACE_TOP);
-            }];
-            [self.messageBackgroundView mas_updateConstraints:^(MASConstraintMaker *make) {
-                make.left.mas_equalTo(self.messageLabel).mas_offset(-MSG_SPACE_LEFT);
-                make.bottom.mas_equalTo(self.messageLabel).mas_offset(MSG_SPACE_BTM);
-            }];
-        } else if (message.ownerTyper == MessageOwnerTypeFriend) {
-            [self.messageBackgroundView setImage:[UIImage imageNamed:@"message_receiver_bg"]];
-            [self.messageBackgroundView setHighlightedImage:[UIImage imageNamed:@"message_receiver_bgHL"]];
-            
-            [self.messageLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
-                make.left.mas_equalTo(self.messageBackgroundView).mas_offset(MSG_SPACE_LEFT);
-                make.top.mas_equalTo(self.messageBackgroundView).mas_offset(MSG_SPACE_TOP);
-            }];
-            [self.messageBackgroundView mas_updateConstraints:^(MASConstraintMaker *make) {
-                make.right.mas_equalTo(self.messageLabel).mas_offset(MSG_SPACE_RIGHT);
-                make.bottom.mas_equalTo(self.messageLabel).mas_offset(MSG_SPACE_BTM);
-            }];
-        }
+    if (message.ownerTyper == MessageOwnerTypeSelf) {
+        [self.messageBackgroundView setImage:[UIImage imageNamed:@"message_sender_bg"]];
+        [self.messageBackgroundView setHighlightedImage:[UIImage imageNamed:@"message_sender_bgHL"]];
+        
+        [self.messageLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+            make.right.mas_equalTo(self.messageBackgroundView).mas_offset(-MSG_SPACE_RIGHT);
+            make.top.mas_equalTo(self.messageBackgroundView).mas_offset(MSG_SPACE_TOP);
+        }];
+        [self.messageBackgroundView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo(self.messageLabel).mas_offset(-MSG_SPACE_LEFT);
+            make.bottom.mas_equalTo(self.messageLabel).mas_offset(MSG_SPACE_BTM);
+        }];
+    } else if (message.ownerTyper == MessageOwnerTypeFriend) {
+        [self.messageBackgroundView setImage:[UIImage imageNamed:@"message_receiver_bg"]];
+        [self.messageBackgroundView setHighlightedImage:[UIImage imageNamed:@"message_receiver_bgHL"]];
+        
+        [self.messageLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo(self.messageBackgroundView).mas_offset(MSG_SPACE_LEFT);
+            make.top.mas_equalTo(self.messageBackgroundView).mas_offset(MSG_SPACE_TOP);
+        }];
+        [self.messageBackgroundView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.right.mas_equalTo(self.messageLabel).mas_offset(MSG_SPACE_RIGHT);
+            make.bottom.mas_equalTo(self.messageLabel).mas_offset(MSG_SPACE_BTM);
+        }];
     }
     
     [self.messageLabel mas_updateConstraints:^(MASConstraintMaker *make) {
