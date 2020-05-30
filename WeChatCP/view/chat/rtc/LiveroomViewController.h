d505f438600606ae3284901a042cfa2306744245
liwuyi <2020783236@qq.com>
2020-05-30 18:11:20 +0800

    视频通信页面

---
+++ WeChatCP/view/chat/rtc/LiveroomViewController.h
@@ -0,0 +1,19 @@
+//
+//  LiveroomViewController.h
+//  WeChatCP
+//
+//  Created by lwy on 2020/5/28.
+//  Copyright © 2020 lwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LiveroomViewController : UIViewController
+
+@property (nonatomic, copy) NSString *token;
+
+@end
+
+NS_ASSUME_NONNULL_END
