69c8a9e9c9983576179d00a76bee9ad47d4edb5e
liwuyi <2020783236@qq.com>
2020-05-30 18:12:50 +0800

    修改地址

--- WeChatCP/utils/api/ApiMacro.h
+++ WeChatCP/utils/api/ApiMacro.h
@@ -9,7 +9,7 @@
 #ifndef ApiMacro_h
 #define ApiMacro_h
 
-#define HOST_URL @"http://localhost:8080/"
+#define HOST_URL @"http://192.168.31.15:8080/"
 #define REGISTER_URL [HOST_URL stringByAppendingString:@"v1/register"]
 
 #endif /* ApiMacro_h */
