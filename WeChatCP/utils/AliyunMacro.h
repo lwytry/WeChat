97e85fcd6e62636eb57146415fdd24c5f4c9d5bc
liwuyi <2020783236@qq.com>
2020-06-02 22:28:00 +0800

    update

--- WeChatCP/utils/AliyunMacro.h
+++ WeChatCP/utils/AliyunMacro.h
@@ -11,8 +11,8 @@
 
 // 请使用自己的 oss 信息
 
-#define     Aliyun_OSS_ACCESSKEY            @"LTAIZ5A6NpqUWSNE";
-#define     Aliyun_OSS_SECRET               @"461CwEDFr3u2dC9baB2xWXvxN1MvCK";
+#define     Aliyun_OSS_ACCESSKEY            @"";
+#define     Aliyun_OSS_SECRET               @"";
 #define     Aliyun_OSS_ENDPOINT             @"oss-cn-hangzhou.aliyuncs.com";
 #define     Aliyun_OSS_BUCKET               @"wechat-cp";
 
